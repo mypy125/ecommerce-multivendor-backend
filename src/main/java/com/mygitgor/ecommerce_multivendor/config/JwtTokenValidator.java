ea876aa9e4b87f160f6c3f3d182cc7d36f249bd4
mohito <mypywelcome@gmail.com>
2025-01-12 14:33:44 +0400

    added_secret-token-validator

--- src/main/java/com/mygitgor/ecommerce_multivendor/config/JwtTokenValidator.java
+++ src/main/java/com/mygitgor/ecommerce_multivendor/config/JwtTokenValidator.java
@@ -1,13 +1,24 @@
 package com.mygitgor.ecommerce_multivendor.config;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.List;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
     @Override
@@ -15,6 +26,26 @@ public class JwtTokenValidator extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException
     {
+        String jwt = request.getHeader(JWT_CONSTANT.JWT_HEADER);
+        if(jwt != null){
+            jwt = jwt.substring(7);
+            try{
+                SecretKey key= Keys.hmacShaKeyFor(JWT_CONSTANT.SECRET_KEY.getBytes());
+                Claims claims = Jwts.parserBuilder().setSigningKey(key).build()
+                        .parseClaimsJws(jwt).getBody();
 
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf(claims.get("authorities"));
+
+                List<GrantedAuthority>auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email,null,auths);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (Exception e) {
+                throw new BadCredentialsException("invalid JWT token...");
+            }
+        }
+        filterChain.doFilter(request,response);
     }
 }
